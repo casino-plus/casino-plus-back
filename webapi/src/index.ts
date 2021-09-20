0db8e26b9a8e9ef7124c12eb6b81cfc1bef518f3
Rubydog <76613417+rbdog@users.noreply.github.com>
2021-09-21 03:26:41 +0900

    herokuデプロイ調整

--- webapi/src/index.ts
+++ webapi/src/index.ts
@@ -12,8 +12,8 @@ app.use(
     next();
   }
 );
-app.listen(3000, () => {
-  console.log("Start on port 3000.");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Now Listen...");
 });
 
 /*** モデル ***/
